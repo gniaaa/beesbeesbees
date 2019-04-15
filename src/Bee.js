872c4042c57b053b5bc3a11e85e7d14c56775cef
eugenia ong <eugenia.osy@gmail.com>
2019-04-15 11:27:27 -0700

    Complete pseudoclassical subclass instantiation

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,11 @@
-var Bee = function() {
+var Bee = function () {
+  Grub.call(this);
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
+
 };
 
+// allows Bee to inherit methods from Grub's prototype
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
